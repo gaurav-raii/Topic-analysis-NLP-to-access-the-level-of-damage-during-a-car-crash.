0e6f991e5d44ad0b6a1b4d9f3accd712c4acdc0e
Gaurav Rai <gauravrai@tamu.edu>
2019-04-14 10:50:59 -0500

    Update Text_Topic_analysis_and_classification.py

--- Text_Topic_analysis_and_classification.py
+++ Text_Topic_analysis_and_classification.py
@@ -8,3 +8,13 @@ Created on Sun Apr 14 10:46:19 2019
 from AdvancedAnalytics import TextAnalytics
 import pandas as pd
 import numpy as np
+
+# Classes for Text Preprocessing 
+import string
+import nltk
+from nltk import pos_tag
+from nltk.tokenize import word_tokenize 
+from nltk.stem.snowball import SnowballStemmer 
+from nltk.stem import WordNetLemmatizer 
+from nltk.corpus import wordnet as wn 
+from nltk.corpus import stopwords
